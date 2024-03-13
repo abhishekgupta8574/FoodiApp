060173ec517d23eda90bcea0f7a17398b7334947
abhishekgupta8574 <abhishekguptaji27@gmail.com>
2024-03-14 01:46:51 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading= React.createElement("h1", {id: "heading"}, "My name is abhishek gupta vicky");
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
